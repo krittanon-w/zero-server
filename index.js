71e8224ff92917bb6123d4e0acfc6841964db92d
krittanon-w <krittanon.w@gmail.com>
2019-07-09 12:11:31 +0700

    add cookie

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const app = express()
-const port = 3000
+const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cookieParser())
+app.get('/', (req, res) => {
+    if (req.cookies == undefined) {
+        count = 0
+    }
+    else if (req.cookies.count == undefined){
+        count = 0
+    }
+    else {
+        count = parseInt(req.cookies.count)
+    }
+    console.log('cookies:', req.cookies)
+    console.log('cookies.count:', count)
+    res.cookie('count', ++count)
+    res.send('ok')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
